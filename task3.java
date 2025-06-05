6a2e82b6c6e212823314a619e9e650b8d5cdc3dd
ABDULMUTHALIF0715 <153576893+ABDULMUTHALIF0715@users.noreply.github.com>
2025-06-05 15:01:04 +0530

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int p=1000;
+        int r=5;
+        int t=2;
+        int si;
+        //Scanner in=new Scanner(system.in);
+        System.out.println("calculating the simple interest");
+        
+        si=(p*r*t)/100;
+        System.out.println(si);
+        // in.close();
+    }
+}
